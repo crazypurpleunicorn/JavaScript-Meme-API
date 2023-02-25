eaf57c526d1ad7e89a9e2f392d4e0d9996f71493
crazypurpleunicorn <santiago.amoros@estudiantat.upc.edu>
2023-02-25 16:36:20 +0100

    Added event listeners to buttons and right/left arrows to scrollthrough meme templates, and given a meme you can costumize it with submit btn

--- api.js
+++ api.js
@@ -4,20 +4,40 @@ let memeId;
 
 function consoleLogFulfilledMemeRequest(template_id, text0, text1) {
   const params = new URLSearchParams();
-  params.append("template_id", template_id);
+  params.append("template_id", "61579");
   params.append("username", "");
   params.append("password", "");
-  params.append("text0", text0);
-  params.append("text1", text1);
+  params.append("text0", "text0");
+  params.append("text1", "text1");
 
   fetch("https://api.imgflip.com/caption_image", {
     method: "POST",
     body: params,
   })
     .then((res) => res.json())
-    .then((data) => () => {
-      return data;
-    });
+    .then((object) => object.data)
+    .then((data) => console.log(data.url));
+}
+
+async function returnUrlGivenMeme(template_id, text0, text1) {
+  const params = new URLSearchParams();
+  params.append("template_id", template_id);
+  params.append("username", "SantiagoAmorsGuilln");
+  params.append("password", "Zxcvbnm,.-123");
+  params.append("text0", text0);
+  params.append("text1", text1);
+
+  const responseObjectFromPromiseValue = await fetch(
+    "https://api.imgflip.com/caption_image",
+    {
+      method: "POST",
+      body: params,
+    }
+  );
+  const objectObjectFromPromise2Value =
+    await responseObjectFromPromiseValue.json();
+  const data = objectObjectFromPromise2Value.data.url;
+  return data;
 }
 
 async function returnArrayOfMemes() {
@@ -34,10 +54,17 @@ function changeImage(memeId, memeArray) {
   document.getElementById("image").src = newUrl;
 }
 
-function changeImageBy1(actualmemeId, memeArray) {
-  memeId = actualmemeId + 1;
-  changeImage(memeId, memeArray);
-  
+function changeImageBy1(memeArray) {
+  if (memeId < memeArray.length - 1) {
+    memeId++;
+    changeImage(memeId, memeArray);
+  }
+}
+function changeImageByMinus1(memeArray) {
+  if (memeId < memeArray.length && memeId > 0) {
+    memeId--;
+    changeImage(memeId, memeArray);
+  }
 }
 
 async function loadFirstMeme() {
@@ -48,6 +75,18 @@ async function loadFirstMeme() {
   document.getElementById("image").src = firstMemeUrl;
 }
 
+function generateRandomIndex(memeArray) {
+  scalar = memeArray.length;
+  randomNumber = Math.floor(Math.random() * scalar);
+  memeId = randomNumber;
+  changeImage(memeId, memeArray);
+}
+
+function giveMeMemeIdFromActualMeme() {
+  let objectiveMeme = memesArray[memeId];
+  let objectiveMemeId = objectiveMeme["id"];
+  return objectiveMemeId;
+}
 /*
 .then(data =>()=>{return data}) 
 
--- eventListeners.js
+++ eventListeners.js
@@ -3,13 +3,35 @@ document.addEventListener("DOMContentLoaded", () => {
   loadFirstMeme();
 });
 
-document.getElementById("previous").addEventListener("click", function () {
-  let buttonPrevious = document.getElementById("previous");
-  buttonPrevious.innerHTML = "hola";
+document.getElementById("previous").addEventListener('click',()=>{
+    changeImageByMinus1(memesArray)
+  });
+
+
+document.getElementById("next").addEventListener('click',()=>{
+    changeImageBy1(memesArray)
+
 });
 
-document.addEventListener("keyup", (event) => {
-  if (event.key === "ArrowDown") {
-    changeImageBy1(memeId, memesArray);
+document.getElementById("random").addEventListener('click',()=>{
+  generateRandomIndex(memesArray)
+  
+})
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowRight") {
+    changeImageBy1(memesArray);
+  }
+  if(event.key==='ArrowLeft'){
+    changeImageByMinus1(memesArray)
   }
 });
+
+document.getElementById('submit-btn').addEventListener('click',async()=>{
+  let text0=document.getElementById('text0').value;
+  let text1=document.getElementById('text1').value;
+  let id=giveMeMemeIdFromActualMeme();
+  let url= await returnUrlGivenMeme(id,text0,text1)
+  document.getElementById('imageresult').src=url
+})
+
